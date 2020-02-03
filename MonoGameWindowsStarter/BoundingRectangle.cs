83fa262dc8fa5cb5c3f17aedd26972c2532fd5ca
Trey <tmoddelmog@ksu.edu>
2020-02-03 14:48:59 -0600

    commit before turning in

---
+++ MonoGameWindowsStarter/BoundingRectangle.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameWindowsStarter
+{
+    public struct BoundingRectangle
+    {
+        public float X, Y, Width, Height;
+
+        public BoundingRectangle(float x, float y, float width, float height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public static implicit operator Rectangle(BoundingRectangle br)
+        {
+            return new Rectangle(
+                (int)br.X,
+                (int)br.Y,
+                (int)br.Width, 
+                (int)br.Height);
+        }
+
+        public bool CollidesWith(BoundingRectangle other)
+        {
+            return !(this.X > other.X + other.Width
+                    || this.X + this.Width < other.X
+                    || this.Y > other.Y + other.Height
+                    || this.Y + this.Height < other.Y);
+        }
+    }
+}
--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MonoGameWindowsStarter
 {
@@ -11,6 +12,24 @@ namespace MonoGameWindowsStarter
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        const int SCREEN_WIDTH = 1042,
+                  SCREEN_HEIGHT = 700,
+                  SHIP_SIZE = 200,
+                  ROCK_SIZE = 100;
+
+        Texture2D space, gameOver;
+        bool isGameOver = false;
+        
+        Texture2D ship;
+        Vector2 shipPosition;
+        int shipSpeed;
+        BoundingRectangle shipBounds;
+        
+        Texture2D rock;
+        Vector2 rockPosition;
+        Vector2 rockVelocity;
+        BoundingRectangle rockBounds;
 
         public Game1()
         {
@@ -27,6 +46,13 @@ namespace MonoGameWindowsStarter
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+            graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
+            graphics.ApplyChanges();
+
+            shipPosition = new Vector2(SCREEN_WIDTH/2 - SHIP_SIZE/2, SCREEN_HEIGHT);
+            rockPosition = new Vector2(0, 0);
+            rockVelocity = new Vector2((float)1, (float)1);
 
             base.Initialize();
         }
@@ -41,6 +67,10 @@ namespace MonoGameWindowsStarter
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ship = Content.Load<Texture2D>("ship");
+            space = Content.Load<Texture2D>("space");
+            rock = Content.Load<Texture2D>("rock");
+            gameOver = Content.Load<Texture2D>("game over");
         }
 
         /// <summary>
@@ -63,6 +93,86 @@ namespace MonoGameWindowsStarter
                 Exit();
 
             // TODO: Add your update logic here
+            shipSpeed = (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            shipBounds = new BoundingRectangle(shipPosition.X,
+                                                shipPosition.Y,
+                                                65,
+                                                65);
+
+            rockBounds = new BoundingRectangle(rockPosition.X,
+                                                rockPosition.Y,
+                                                50,
+                                                50);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                shipPosition.Y -= shipSpeed;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                shipPosition.Y += shipSpeed;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                shipPosition.X -= shipSpeed;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                shipPosition.X += shipSpeed;
+
+            // ship check for wall collisions
+            // top
+            if (shipPosition.Y < -75)
+                shipPosition.Y = -75;
+            // bottom
+            if (shipPosition.Y > graphics.PreferredBackBufferHeight - 125)
+                shipPosition.Y = graphics.PreferredBackBufferHeight - 125;
+            // left
+            if (shipPosition.X < -70)
+                shipPosition.X = -70;
+            // right
+            if (shipPosition.X > graphics.PreferredBackBufferWidth - 130)
+                shipPosition.X = graphics.PreferredBackBufferWidth - 130;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                rockVelocity.Y += 1;
+                rockVelocity.X += 1;
+                if (isGameOver) isGameOver = false;
+            }
+
+            rockPosition += rockVelocity;
+
+            // rock check for wall collisions
+            // top
+            if (rockPosition.Y < 0)
+            {
+                rockVelocity.Y *= -1;
+                float delta = 0 - rockPosition.Y;
+                rockPosition.Y += 2 * delta;
+            }
+            // bottom
+            if (rockPosition.Y > graphics.PreferredBackBufferHeight - ROCK_SIZE)
+            {
+                rockVelocity.Y *= -1;
+                float delta = graphics.PreferredBackBufferHeight - ROCK_SIZE - rockPosition.Y;
+                rockPosition.Y += 2 * delta;
+            }
+            // left
+            if (rockPosition.X < 0)
+            {
+                rockVelocity.X *= -1;
+                float delta = 0 - rockPosition.X;
+                rockPosition.X += 2 * delta;
+            }
+            // right
+            if (rockPosition.X > graphics.PreferredBackBufferWidth - ROCK_SIZE)
+            {
+                rockVelocity.X *= -1;
+                float delta = graphics.PreferredBackBufferWidth - ROCK_SIZE - rockPosition.X;
+                rockPosition.X += 2 * delta;
+            }
+
+            if (shipBounds.CollidesWith(rockBounds))
+            {
+                rockVelocity.X = 0;
+                rockVelocity.Y = 0;
+                isGameOver = true;
+            }
 
             base.Update(gameTime);
         }
@@ -76,6 +186,41 @@ namespace MonoGameWindowsStarter
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            // draw background
+            spriteBatch.Draw(space, 
+                new Rectangle(0,0, SCREEN_WIDTH, SCREEN_HEIGHT), 
+                Color.White);
+
+            // draw ship
+            spriteBatch.Draw(ship, 
+                new Rectangle((int)shipPosition.X,
+                                (int)shipPosition.Y, 
+                                SHIP_SIZE, 
+                                SHIP_SIZE),
+                Color.White);
+
+            // draw rock
+            spriteBatch.Draw(rock,
+                new Rectangle((int)rockPosition.X,
+                                (int)rockPosition.Y,
+                                ROCK_SIZE,
+                                ROCK_SIZE),
+                Color.White);
+
+            // draw game over if game is over
+            if (isGameOver)
+            {
+                spriteBatch.Draw(gameOver,
+                    new Rectangle((int)SCREEN_WIDTH/2 - 100,
+                                    (int)SCREEN_HEIGHT/2 - 100,
+                                    200,
+                                    200),
+                    Color.White);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
